7e162cbe53ddc44ac3963bd68fa655110d86ab2c
Averkiev91 <amd3500m@gmail.com>
2022-09-25 22:28:25 +0300

    webpack dev-server fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const TerserPlugin = require('terser-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: "./src/js/index.js",
+    entry: ["@babel/polyfill", "./src/js/index.js"],
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
     },
     devServer: {
         static : {
             directory: path.join(__dirname, 'dist')
         },
+        watchFiles: ["./src/*"],
         compress: true,
         port: 9999
     },
@@ -35,13 +30,51 @@ module.exports = {
             {
                 filename: "style.css",
             }
-        )
+        ),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "src/img",
+                    to: "img",
+                    globOptions: {
+                        ignore: ['*.DS_Store'],
+                    },
+                },
+            ],
+        })
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    },
     module: {
         rules: [
+            // Styles: Inject CSS into the head with source maps
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            // Images: Copy image files to build folder
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i
+            },
+            // Fonts and SVGs: Inline files
+            {
+                test: /\.(ttf|eot|svg|woff2?)$/i
+            },
+            // JavaScript: Use Babel to transpile JavaScript files
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
