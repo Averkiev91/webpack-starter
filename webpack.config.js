62bb73818a48cfe5ce6d4cd2a2e9eee445f0a2c9
Averkiev91 <amd3500m@gmail.com>
2022-09-25 20:16:06 +0300

    add webpack plugins css loader, min.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/js/index.js",
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin(
@@ -13,6 +23,19 @@ module.exports = {
                 filename: "index.html",
                 template: "./src/index.html"
             }
+        ),
+        new MiniCssExtractPlugin(
+            {
+                filename: "style.css",
+            }
         )
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    }
 }
