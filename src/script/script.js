147eb6c149f8585934475b670c8f06fe1ab5a850
Averkiev91 <amd3500m@gmail.com>
2022-10-19 14:41:27 +0300

    finish

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,19 @@
 import '../index.html'
 import '../style/style.css' // Импортируем стили css
 
+document.addEventListener("DOMContentLoaded", () => {
 
-console.log("Hello world!")
+    const inputs = document.querySelectorAll('.input__file'),
+        success = document.querySelectorAll('.input__success'),
+        pencil = document.querySelectorAll(' .input__file-icon-pencil'),
+        plus = document.querySelectorAll(' .input__file-icon')
+
+    inputs.forEach((item, index) => {
+        item.addEventListener('change', function (e) {
+            if (this.files && this.files.length >= 1)
+                success[index].style.display = 'block';
+                pencil[index].style.display = 'block';
+                plus[index].style.display = 'none';
+        })
+    })
+})
