fcf13395373b989bd7d8907f3f26076146c04673
Averkiev91 <amd3500m@gmail.com>
2023-03-25 19:25:32 +0300

    hero section

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,36 @@
 import '../index.html'
 import 'bootstrap/dist/css/bootstrap.css';
-import '../style/style.css' // Импортируем стили css
+import '../style/style.css';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Swiper, {Autoplay, Navigation, Pagination} from 'swiper';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper(".hero-slider", {
+        modules: [Navigation, Pagination, Autoplay],
+        pagination: {
+            el: ".swiper-pagination",
+            type: 'bullets',
+            clickable: true
+        },
+        autoplay: {
+            delay: 6000,
+            disableOnInteraction: false
+        },
+        navigation: {
+            nextEl: '.hero__next',
+            prevEl: '.hero__prev',
+        },
+        on: {
+            init: function () {
+                const paginationBullets = document.querySelectorAll('.hero__pag .swiper-pagination-bullet');
+
+                paginationBullets.forEach(el => {
+                    el.innerHTML = `<span class="hero__bar"></span>`;
+                });
+            },
+        }
+    });
+})
