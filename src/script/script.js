08df60d17cf69541325a64e1832387b7a29f9360
Averkiev91 <amd3500m@gmail.com>
2023-03-26 17:04:25 +0300

    max-width 1024px

--- src/script/script.js
+++ src/script/script.js
@@ -60,4 +60,39 @@ document.addEventListener("DOMContentLoaded", () => {
         let y = window.scrollY;
         isVisibleToTop(y);
     });
+
+
+    let burger = document.querySelector('.burger');
+    let menu = document.querySelector('.header__nav');
+    let overlay = document.querySelector('.overlay');
+
+    let disScroll = function disScroll() {
+        let pagePosition = window.scrollY;
+        document.body.classList.add('dis-scroll');
+        document.body.dataset.position = pagePosition;
+        document.body.style.top = -pagePosition + 'px';
+    };
+
+    let enScroll = function enScroll() {
+        let pagePosition = parseInt(document.body.dataset.position, 10);
+        document.body.style.top = 'auto';
+        document.body.classList.remove('dis-scroll');
+        window.scrollTo({
+            top: pagePosition,
+            left: 0
+        });
+        document.body.removeAttribute('data-position');
+    };
+
+    burger.addEventListener('click', function (e) {
+        burger.classList.toggle('burger--active');
+        menu.classList.toggle('header__nav--active');
+        overlay.classList.toggle('overlay--active');
+
+        if (burger.classList.contains('burger--active')) {
+            disScroll();
+        } else {
+            enScroll();
+        }
+    });
 })
